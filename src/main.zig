b9001d6e1250a4e2a976d7ee7124dcf290b3727b
Michael Lynch <git@mtlynch.io>
2023-11-26 13:02:14 -0500

    Create a simple driver Zig app

---
+++ src/main.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+// Import the base64 implementation from uStreamer's C source file.
+const ustreamer = @cImport({
+    @cInclude("libs/base64.c");
+});
+
+pub fn main() !void {
+    // Create a standard Zig string.
+    const input = "hello, world!";
+
+    // Create variables to store the ouput parameters of us_base64_encode.
+    var cEncoded: [*c]u8 = null;
+    var allocatedSize: usize = 0;
+
+    // Call the uStreamer C function from Zig.
+    ustreamer.us_base64_encode(input.ptr, input.len, &cEncoded, &allocatedSize);
+    defer std.c.free(cEncoded);
+
+    // Print the input and output of the base64 encode operation.
+    std.debug.print("input:  {s}\n", .{input});
+    std.debug.print("output: {s}\n", .{cEncoded});
+}
