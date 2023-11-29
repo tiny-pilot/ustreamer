dafedcc1d8900a7b110d832206e2bb03cf7ab387
Michael Lynch <git@mtlynch.io>
2023-11-29 10:00:46 -0500

    Simplify test.c

--- src/test.c
+++ src/test.c
@@ -2,16 +2,12 @@
 
 #include "libs/base64.h"
 
-int main(void)
-{
-  char *raw_token = "hello, world!";
-  char *encoded_token = NULL;
-  us_base64_encode((uint8_t *)raw_token, strlen(raw_token), &encoded_token, NULL);
-  printf("encoded=%s\n", encoded_token);
-  for (int i = 0; i < strlen(encoded_token); i++) {
-    printf("%d, ", encoded_token[i]);
-  }
-  printf("\n");
-  free(encoded_token);
-  return 0;
+void main(void) {
+  char *raw = "hello, world!";
+  char *encoded = NULL;
+  size_t encoded_bytes = 0;
+  us_base64_encode((uint8_t *)raw, strlen(raw), &encoded, &encoded_bytes);
+  printf("encoded=%s\n", encoded);
+  printf("encoded_bytes=%u\n", encoded_bytes);
+  free(encoded);
 }
