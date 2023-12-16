d068c0e2fc5a1a8735856ddc2f922ff45a39c409
Michael Lynch <git@mtlynch.io>
2023-12-16 11:20:59 -0500

    Refactor base64 logic into wrapper function

--- src/main.zig
+++ src/main.zig
@@ -5,22 +5,46 @@ const ustreamer = @cImport({
     @cInclude("libs/base64.c");
 });
 
-pub fn main() !void {
-    // Create a standard Zig string.
-    const input = "hello, world!";
-
-    // Create variables to store the ouput parameters of us_base64_encode.
+fn base64Encode(allocator: std.mem.Allocator, data: []const u8) ![:0]u8 {
     var cEncoded: [*c]u8 = null;
     var allocatedSize: usize = 0;
 
-    // Call the uStreamer C function from Zig.
-    ustreamer.us_base64_encode(input.ptr, input.len, &cEncoded, &allocatedSize);
-
-    // Free the memory that the C function allocated when this function exits.
+    ustreamer.us_base64_encode(data.ptr, data.len, &cEncoded, &allocatedSize);
     defer std.c.free(cEncoded);
+
+    // The length of the string excludes the null-terminator, so subtract 1.
+    const cEncodedLength = allocatedSize - 1;
+    return cStringToZigString(allocator, cEncoded, cEncodedLength);
+}
+
+fn cStringToZigString(allocator: std.mem.Allocator, cString: [*c]const u8, cStringLength: usize) ![:0]u8 {
+    // Allocate a Zig-managed buffer to contain the contents of cString.
+    const zigString = try allocator.allocSentinel(u8, cStringLength, 0);
+
+    // If we can't return the result, free the memory we allocated.
+    errdefer allocator.free(zigString);
+
+    // Create a Zig slice of cString, and declare to Zig that the slice ends
+    // with a null terminator.
+    const cStringSlice = cString[0..cStringLength :0];
+
+    // Copy the contents of the C string into the Zig slice.
+    @memcpy(zigString.ptr, cStringSlice);
+
+    return zigString;
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    const input = "hello, world!";
+    const output = try base64Encode(allocator, input);
+    defer allocator.free(output);
 
     // Print the input and output of the base64 encode operation.
     std.debug.print("input:       {s}\n", .{input});
-    std.debug.print("output:      {s}\n", .{cEncoded});
-    std.debug.print("output size: {d}\n", .{allocatedSize});
+    std.debug.print("output:      {s}\n", .{output});
+    std.debug.print("output size: {d}\n", .{output.len});
 }
