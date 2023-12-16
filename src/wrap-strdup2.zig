cc9b4224319edad372b4ee17ae8a85142514f25c
Michael Lynch <git@mtlynch.io>
2023-12-16 16:31:19 -0500

    Simplify strdup

--- src/wrap-strdup2.zig
+++ src/wrap-strdup2.zig
@@ -4,18 +4,10 @@ const cString = @cImport({
     @cInclude("string.h");
 });
 
-const StrdupError = error{
-    StrdupFailure,
-};
-
 fn strdup(allocator: std.mem.Allocator, str: [:0]const u8) ![:0]u8 {
-    const cCopy: [*c]u8 = cString.strdup(str);
-    if (cCopy == null) {
-        // Maybe we can return a better error by calling std.os.errno(), but for
-        // now, return a generic error.
-        return error.StrdupFailure;
-    }
+    const cCopy: [*:0]u8 = cString.strdup(str) orelse return error.OutOfMemory;
     defer std.c.free(cCopy);
+
     const zCopy: [:0]u8 = std.mem.span(cCopy);
     const copy: [:0]u8 = try allocator.allocSentinel(u8, zCopy.len, 0);
     @memcpy(copy, zCopy);
