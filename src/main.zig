75b3294346526d6bf548cf7cac361408e29ac62d
Michael Lynch <git@mtlynch.io>
2023-12-17 12:55:11 -0500

    Get better typing

--- src/main.zig
+++ src/main.zig
@@ -10,17 +10,20 @@ pub fn main() !void {
     const input = "hello, world!";
 
     // Create variables to store the ouput parameters of us_base64_encode.
-    var cEncoded: [*c]u8 = null;
+    var cEncoded: ?[*:0]u8 = null;
     var allocatedSize: usize = 0;
 
     // Call the uStreamer C function from Zig.
     ustreamer.us_base64_encode(input.ptr, input.len, &cEncoded, &allocatedSize);
+
+    // Get the output as a non-optional type.
+    const output: [*:0]u8 = cEncoded orelse return error.UnexpectedNull;
 
     // Free the memory that the C function allocated when this function exits.
     defer std.c.free(cEncoded);
 
     // Print the input and output of the base64 encode operation.
     std.debug.print("input:       {s}\n", .{input});
-    std.debug.print("output:      {s}\n", .{cEncoded});
+    std.debug.print("output:      {s}\n", .{output});
     std.debug.print("output size: {d}\n", .{allocatedSize});
 }
